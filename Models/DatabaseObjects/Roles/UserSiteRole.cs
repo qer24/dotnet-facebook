11e9c1ab271a2e464089c8b5db9e297516b19dbe
qer24 <qer24@go2.pl>
2024-04-13 19:30:03 +0200

    Fix typo

--- Models/DatabaseObjects/Roles/UserSiteRole.cs
+++ Models/DatabaseObjects/Roles/UserSiteRole.cs
@@ -1,12 +1,14 @@
 ﻿using dotnet_facebook.Models.DatabaseObjects.Users;
+using System.ComponentModel.DataAnnotations;
 
 namespace dotnet_facebook.Models.DatabaseObjects.Roles
 {
     public class UserSiteRole
     {
-        public int UserSireRoleId { get; set; }
-        public virtual User User { get; set; }
+        [Key]
+        public int UserSiteRoleId { get; set; }
 
+        public virtual User User { get; set; }
         public virtual SiteRole Role { get; set; }
     }
 }
