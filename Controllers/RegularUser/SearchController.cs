e4bac3aac3113bd96f6418a8776093b55d1ece6e
qer24 <qer24@go2.pl>
2024-06-02 00:00:28 +0200

    Search Front End

---
+++ Controllers/RegularUser/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using dotnet_facebook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_facebook.Controllers.RegularUser
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        [HttpGet]
+        public IActionResult Index(string q)
+        {
+            if (string.IsNullOrEmpty(q))
+            {
+                return RedirectToAction("Index", "UserHome");
+            }
+
+            var searchModel = new SearchModel
+            {
+                Query = q
+            };
+
+            return View(searchModel);
+        }
+    }
+}
---
+++ Models/SearchModel.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_facebook.Models.DatabaseObjects.Groups;
+using dotnet_facebook.Models.DatabaseObjects.Posts;
+using dotnet_facebook.Models.DatabaseObjects.Users;
+using System.Collections;
+
+namespace dotnet_facebook.Models;
+
+public class SearchModel
+{
+    public string Query { get; set; }
+
+    public List<MainPost> Posts { get; set; }
+    public List<User> Users { get; set; }
+    public List<Group> Groups { get; set; }
+}
