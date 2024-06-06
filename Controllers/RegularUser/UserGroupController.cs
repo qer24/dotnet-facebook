42c80a6fd809402746a7f5c87811507379cd692f
kruchelak <domin.kruchelski@gmail.com>
2024-06-06 22:54:24 +0200

    am i a retard

--- Controllers/RegularUser/UserGroupController.cs
+++ Controllers/RegularUser/UserGroupController.cs
@@ -3,6 +3,7 @@ using dotnet_facebook.Models.Contexts;
 using dotnet_facebook.Models.DatabaseObjects.Groups;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
@@ -41,10 +42,101 @@ namespace dotnet_facebook.Controllers.RegularUser
             }
             var userRole = group.Users.FirstOrDefault(u => u.User.Nickname == User.Identity.Name)?.GroupRole;
 
-
+            ViewBag.localUser = User.FindFirstValue(ClaimTypes.NameIdentifier);
             ViewBag.IsAdmin = userRole == GroupRole.Admin;
             ViewBag.IsMember = userRole == GroupRole.Member;
             return View(group);
+        }
+        public async Task<IActionResult> AddUser(int? id)
+        {
+            if (id == null)
+            {
+                return GroupNotFound();
+            }
+
+            var @group = await _context.Groups
+                .Include(g => g.Users)
+                .ThenInclude(gu => gu.User)
+                .FirstOrDefaultAsync(m => m.GroupId == id);
+            if (@group == null)
+            {
+                return GroupNotFound();
+            }
+
+            var userStringError = "";
+
+            // if user is already in the group, throw error
+            var selectedUserIdString = Request.Form["selectedUserId"];
+            if (string.IsNullOrWhiteSpace(selectedUserIdString))
+            {
+                userStringError = "Please select a user.";
+            }
+            else
+            {
+                var userAlreadyInGroup = @group.Users.Any(gu => gu.User.UserId == Convert.ToInt32(selectedUserIdString));
+
+                if (userAlreadyInGroup)
+                {
+                    userStringError = "User is already in the group.";
+                }
+                else
+                {
+                    var user = _context.Users.Find(Convert.ToInt32(selectedUserIdString));
+                    @group.Users.Add(new GroupUser
+                    {
+                        User = user,
+                        Group = @group,
+                        GroupRole = GroupRole.Member
+                    });
+
+                    try
+                    {
+                        _context.Update(@group);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!GroupExists(@group.GroupId))
+                        {
+                            return GroupNotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+
+            GenerateUsersBag();
+            GenerateRolesBag();
+
+            return GroupNotFound();
+        }
+
+        private bool GroupExists(int id)
+        {
+            return _context.Groups.Any(e => e.GroupId == id);
+        }
+
+
+
+        private void GenerateUsersBag()
+        {
+            ViewBag.Users = _context.Users.Select(u => new SelectListItem
+            {
+                Value = u.UserId.ToString(),
+                Text = u.Nickname
+            }).ToList();
+        }
+
+        private void GenerateRolesBag()
+        {
+            ViewBag.Roles = Enum.GetValues(typeof(GroupRole)).Cast<GroupRole>().Select(r => new SelectListItem
+            {
+                Value = r.ToString(),
+                Text = r.ToString()
+            }).ToList();
         }
         [HttpGet("GroupNotFound")]
         public IActionResult GroupNotFound()
